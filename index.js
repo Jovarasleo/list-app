54af91c46abc131b86af6d2c9254061b9d141cd5
Jovaras Leonavicius <52199839+jowarnis@users.noreply.github.com>
2021-09-28 21:51:36 +0300

    second commit

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const button1 = document.querySelector(".btn-adduser-unshift");
 const button2 = document.querySelector(".btn-adduser-shift");
 const button3 = document.querySelector(".btn-adduser-pop");
 const button4 = document.querySelector(".btn-reverse");
-const result = document.querySelector("body");
+const result = document.querySelector(".output");
 
 function draw() {
-  let newli = document.createElement("li");
+  result.innerHTML = null;
   for (var i = 0; i < userlist.length; i++) {
+    let newli = document.createElement("li");
     let text = document.createTextNode(userlist[i]);
     newli.appendChild(text);
-    result.appendChild(newli).innerHTML += ", ";
+    result.appendChild(newli);
   }
 }
 button.addEventListener("click", () => {
@@ -24,14 +25,14 @@ button1.addEventListener("click", () => {
   draw();
 });
 button2.addEventListener("click", () => {
-  userlist.shift(input.value);
+  userlist.shift(input);
   draw();
 });
 button3.addEventListener("click", () => {
-  userlist.pop(input.value);
+  userlist.pop(input);
   draw();
 });
 button4.addEventListener("click", () => {
-  userlist.reverse(input.value);
+  userlist.reverse(input);
   draw();
 });
