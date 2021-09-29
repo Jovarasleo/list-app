789cd20f61e00d5bb7997061b7fa6a751c5abc2d
Jovaras Leonavicius <52199839+jowarnis@users.noreply.github.com>
2021-09-29 21:33:38 +0300

    update 5 - added output generation

--- index.js
+++ index.js
@@ -2,67 +2,69 @@ let userlist = [];
 let start = document.querySelector(".start");
 let delnum = document.querySelector(".delnum");
 let input = document.querySelector(".name");
-const result = document.querySelector(".output");
+const result = document.querySelector("body");
 
 function draw() {
-  result.innerHTML = null;
-  var element = document.querySelector(".mystyle");
-  if (typeof (element == "undefined" && element == null)) {
-    let newol = document.createElement("ol");
-    newol.classList.add("mystyle");
-    for (var i = 0; i < userlist.length; i++) {
-      let newli = document.createElement("li");
-      let text = document.createTextNode(userlist[i]);
-      newol.appendChild(newli);
-      newli.appendChild(text);
-      result.appendChild(newol);
+  const maxresult =
+    document.querySelector(".output") || document.createElement("section");
+  if (typeof (maxresult == "undefined" && maxresult == null)) {
+    maxresult.classList.add("output");
+    maxresult.innerHTML = null;
+    let element = document.querySelector(".mystyle");
+    if (typeof (element == "undefined" && element == null)) {
+      let newol = document.createElement("ol");
+      newol.classList.add("mystyle");
+      for (let i = 0; i < userlist.length; i++) {
+        let newli = document.createElement("li");
+        let text = document.createTextNode(userlist[i]);
+        newol.appendChild(newli);
+        newli.appendChild(text);
+        maxresult.appendChild(newol);
+        result.appendChild(maxresult);
+      }
     }
   }
 }
-
-function select(className) {
-  if (input.value != null && input.value != "") {
-    switch (className) {
-      case "btn-adduser-push":
-        userlist.push(input.value);
-        break;
-      case "btn-adduser-unshift":
-        userlist.unshift(input.value);
-        break;
-      case "btn-adduser-shift":
-        userlist.shift();
-        break;
-      case "btn-adduser-pop":
-        userlist.pop();
-        break;
-      case "btn-reverse":
-        userlist.reverse();
-        break;
-      case "btn-splice-remove":
-        if (delnum.value == 0) {
-          delnum.value = 1;
-        }
-        userlist.splice(start.value - 1, delnum.value);
-        break;
-      case "btn-splice-add":
-        userlist.splice(start.value - 1, delnum.value, input.value);
-        break;
-      case "moveUp":
-        let b = userlist.shift();
-        userlist.push(b);
-        break;
-      case "moveDown":
-        let a = userlist.pop();
-        userlist.unshift(a);
-        break;
-      default:
-    }
-    draw();
-  }
-}
-let buttons = document.querySelector("body");
-buttons.addEventListener("click", (event) => {
+document.querySelector("body").addEventListener("click", (event) => {
   if (event.target.tagName === "BUTTON") {
-    select(event.target.className);
+    if (input.value != null && input.value != "") {
+      switch (event.target.className) {
+        case "btn-adduser-push":
+          userlist.push(input.value);
+          break;
+        case "btn-adduser-unshift":
+          userlist.unshift(input.value);
+          break;
+        case "btn-adduser-shift":
+          userlist.shift();
+          break;
+        case "btn-adduser-pop":
+          userlist.pop();
+          break;
+        case "btn-reverse":
+          userlist.reverse();
+          break;
+        case "btn-splice-remove":
+          if (delnum.value == 0) {
+            delnum.value = 1;
+          }
+          userlist.splice(start.value - 1, delnum.value);
+          break;
+        case "btn-splice-add":
+          userlist.splice(start.value - 1, delnum.value, input.value);
+          break;
+        case "moveUp":
+          let b = userlist.shift();
+          userlist.push(b);
+          break;
+        case "moveDown":
+          let a = userlist.pop();
+          userlist.unshift(a);
+          break;
+        default:
+          break;
+      }
+      draw();
+    }
   }
 });
