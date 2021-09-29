561fa951eafc8d4191004b64e4d4aba3aecb7d07
Jovaras Leonavicius <52199839+jowarnis@users.noreply.github.com>
2021-09-29 19:46:16 +0300

    update2

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
 let userlist = [];
+let start = document.querySelector(".start");
+let delnum = document.querySelector(".delnum");
 let input = document.querySelector(".name");
-const button = document.querySelector(".btn-adduser-push");
-const button1 = document.querySelector(".btn-adduser-unshift");
-const button2 = document.querySelector(".btn-adduser-shift");
-const button3 = document.querySelector(".btn-adduser-pop");
-const button4 = document.querySelector(".btn-reverse");
 const result = document.querySelector(".output");
 
 function draw() {
   result.innerHTML = null;
-  for (var i = 0; i < userlist.length; i++) {
-    let newli = document.createElement("li");
-    let text = document.createTextNode(userlist[i]);
-    newli.appendChild(text);
-    result.appendChild(newli);
+  var element = document.querySelector(".mystyle");
+  if (typeof (element == "undefined" && element == null)) {
+    let newol = document.createElement("ol");
+    newol.classList.add("mystyle");
+    for (var i = 0; i < userlist.length; i++) {
+      let newli = document.createElement("li");
+      let text = document.createTextNode(userlist[i]);
+      newol.appendChild(newli);
+      newli.appendChild(text);
+      result.appendChild(newol);
+    }
   }
 }
-button.addEventListener("click", () => {
-  userlist.push(input.value);
-  draw();
-});
-button1.addEventListener("click", () => {
-  userlist.unshift(input.value);
-  draw();
-});
-button2.addEventListener("click", () => {
-  userlist.shift(input);
-  draw();
-});
-button3.addEventListener("click", () => {
-  userlist.pop(input);
-  draw();
-});
-button4.addEventListener("click", () => {
-  userlist.reverse(input);
-  draw();
+
+function select(className) {
+  switch (className) {
+    case "btn-adduser-push":
+      userlist.push(input.value), draw();
+      break;
+    case "btn-adduser-unshift":
+      userlist.unshift(input.value), draw();
+      break;
+    case "btn-adduser-shift":
+      userlist.shift(), draw();
+      break;
+    case "btn-adduser-pop":
+      userlist.pop(), draw();
+      break;
+    case "btn-reverse":
+      userlist.reverse(), draw();
+      break;
+    case "btn-splice-remove":
+      if (delnum.value == 0) {
+        delnum.value = 1;
+      }
+      userlist.splice(start.value, delnum.value), draw();
+      break;
+    case "btn-splice-add":
+      userlist.splice(start.value, delnum.value, input.value), draw();
+      break;
+    case "moveUp":
+      let b = userlist.shift();
+      userlist.push(b), draw();
+      break;
+    case "moveDown":
+      let a = userlist.pop();
+      userlist.unshift(a), draw();
+      break;
+    default:
+  }
+}
+let buttons = document.querySelector("body");
+buttons.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    select(event.target.className);
+  }
 });
